d443808d1ddd246dc049a38b4f7d82a4526bba70
Alec Jones <alecaej2002@gmail.com>
2020-07-06 16:16:09 -0700

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -4,17 +4,17 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Alec Jones</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Hey, I'm Alec
         </h1>
 
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          I'm an 18-year-old developer from Canada 🇨🇦
         </p>
 
         <div className="grid">
